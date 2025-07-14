035daae555269b4d7450845758c62c61087a7635
Гулевич Екатерина <meteorgul@gmail.com>
2025-07-14 17:56:32 +0300

    fix reconnect to socket

--- src/socket.ts
+++ src/socket.ts
@@ -10,9 +10,9 @@ export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
   WS_URL,
   {
     autoConnect: false,
-    auth: {
-      // todo: работает только при обновлении страницы, но не работает при переключении между страницами через лого
-      userId: sessionStorage.getItem(SESSION_KEY_USER_ID),
-    },
+    auth: (cb) =>
+      cb({
+        userId: sessionStorage.getItem(SESSION_KEY_USER_ID),
+      }),
   },
 );
