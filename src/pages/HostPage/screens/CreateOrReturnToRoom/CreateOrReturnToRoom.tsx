cdddc3d8bdab707429e8917fc2e336d7306f09f9
Гулевич Екатерина <meteorgul@gmail.com>
2025-07-18 02:36:30 +0300

    float button

--- src/pages/HostPage/screens/CreateOrReturnToRoom/CreateOrReturnToRoom.tsx
+++ src/pages/HostPage/screens/CreateOrReturnToRoom/CreateOrReturnToRoom.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { ReloadOutlined, UsergroupAddOutlined } from "@ant-design/icons";
-import { Button, Result } from "antd";
+import { Button, Col, Result, Row } from "antd";
 
 type CreateOrReturnToRoomProps = {
   onCreateRoom: () => void;
@@ -16,32 +16,40 @@ export const CreateOrReturnToRoom: FC<CreateOrReturnToRoomProps> = ({
 
   if (typeof onReenterRoom === "function") {
     return (
-      <Result
-        icon={<ReloadOutlined />}
-        title={t("createOrReturnToRoom.returnResult.title")}
-        subTitle={t("createOrReturnToRoom.returnResult.subTitle")}
-        extra={[
-          <Button key={"create"} onClick={onReenterRoom} type={"primary"}>
-            {t("createOrReturnToRoom.returnResult.buttons.return")}
-          </Button>,
-          <Button key={"return"} onClick={onCreateRoom}>
-            {t("createOrReturnToRoom.returnResult.buttons.createNew")}
-          </Button>,
-        ]}
-      />
+      <Row justify={"center"}>
+        <Col span={12}>
+          <Result
+            icon={<ReloadOutlined />}
+            title={t("createOrReturnToRoom.returnResult.title")}
+            subTitle={t("createOrReturnToRoom.returnResult.subTitle")}
+            extra={[
+              <Button key={"create"} onClick={onReenterRoom} type={"primary"}>
+                {t("createOrReturnToRoom.returnResult.buttons.return")}
+              </Button>,
+              <Button key={"return"} onClick={onCreateRoom}>
+                {t("createOrReturnToRoom.returnResult.buttons.createNew")}
+              </Button>,
+            ]}
+          />
+        </Col>
+      </Row>
     );
   }
 
   return (
-    <Result
-      icon={<UsergroupAddOutlined />}
-      title={t("createOrReturnToRoom.createResult.title")}
-      subTitle={t("createOrReturnToRoom.createResult.subTitle")}
-      extra={
-        <Button onClick={onCreateRoom}>
-          {t("createOrReturnToRoom.createResult.buttons.create")}
-        </Button>
-      }
-    />
+    <Row justify={"center"}>
+      <Col span={12}>
+        <Result
+          icon={<UsergroupAddOutlined />}
+          title={t("createOrReturnToRoom.createResult.title")}
+          subTitle={t("createOrReturnToRoom.createResult.subTitle")}
+          extra={
+            <Button onClick={onCreateRoom}>
+              {t("createOrReturnToRoom.createResult.buttons.create")}
+            </Button>
+          }
+        />
+      </Col>
+    </Row>
   );
 };
