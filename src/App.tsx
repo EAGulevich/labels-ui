966f8e51874867c2bb93677a56eca6101293efd3
Гулевич Екатерина <meteorgul@gmail.com>
2025-07-19 02:06:38 +0300

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
-import { ConfigProvider as AntConfigProvider } from "antd";
+import { ConfigProvider as AntConfigProvider, Layout } from "antd";
 
 import { Header } from "@components/Header/Header.tsx";
 import { FOOTER_CONTENT, ROUTE_PATHS } from "@constants";
 import { useAppSettings } from "@providers/AppSettingsProvider/AppSettingsProvider.tsx";
-import { GameStateProvider } from "@providers/GameStateProvider.tsx";
 import { ThemeTokenProvider } from "@providers/ThemeTokenProvider.tsx";
 
+import { ResolutionWarning } from "./pages/HostPage/parts/ResolutionWarning.tsx";
 import { Pages } from "./pages/Pages.tsx";
 import { getThemeByName } from "./theme/theme.tsx";
-import { StyledContent, StyledFooter, StyledPageLayout } from "./AppStyles.ts";
+import { SContent, SLayout, StickyHeader } from "./AppStyles.ts";
 
 function App() {
   const [layoutHeight, setLayoutHeight] = useState(window.innerHeight);
@@ -28,15 +28,16 @@ function App() {
   return (
     <AntConfigProvider theme={getThemeByName(themeName)}>
       <ThemeTokenProvider>
-        <GameStateProvider>
-          <StyledPageLayout height={layoutHeight + "px"}>
-            <Header onlyMenuButton={location.pathname === ROUTE_PATHS.home} />
-            <StyledFooter id={FOOTER_CONTENT} />
-            <StyledContent>
-              <Pages />
-            </StyledContent>
-          </StyledPageLayout>
-        </GameStateProvider>
+        <SLayout height={layoutHeight + "px"}>
+          <StickyHeader>
+            <Header hasOnlyMenu={location.pathname === ROUTE_PATHS.home} />
+          </StickyHeader>
+          <ResolutionWarning />
+          <Layout.Footer id={FOOTER_CONTENT} />
+          <SContent>
+            <Pages />
+          </SContent>
+        </SLayout>
       </ThemeTokenProvider>
     </AntConfigProvider>
   );
